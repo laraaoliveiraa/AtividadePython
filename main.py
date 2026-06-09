313c2a037a7bf2e32dd6afe72f8411d90459829f
laraaoliveiraa <laraoliveira.senai@gmail.com>
2026-06-09 15:04:31 -0300

    Atividade inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+numAnterior = 0
+
+for numAtual in range(10):
+    soma = numAtual + numAnterior
+    print("Número atual:", numAtual, "Número anterior", numAnterior, "Soma:", soma)
+    numAnterior = numAtual
+
